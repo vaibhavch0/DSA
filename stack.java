7a492e5752c0112837948d1e59c65904edfaa30a
Vaibhav Choudhary <vaibhavchaudhary308@gmail.com>
2025-10-17 22:30:04 +0530

    503 leetcode

--- stack.java
+++ stack.java
@@ -25,3 +25,32 @@ isme hmne stack ko sort krna h to sbse phle hmne stack ko pop krke ek arraylist 
 fir hmne arraylist ko sort kr diya
 fir hmne arraylist ke elements ko stack m push kr diya
 */
+
+//503 . next greater element II
+/*
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n];
+        Arrays.fill(arr, -1);
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int num = nums[i % n];
+            while (!st.isEmpty() && st.peek() <= num) {
+                st.pop();
+            }
+            if (i < n && !st.isEmpty()) {
+                arr[i] = st.peek();
+            }
+            st.push(num);
+        }
+
+        return arr;
+    }
+}
+
+
+is code e
+*/  
